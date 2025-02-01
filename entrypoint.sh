d3ea6df0e1a51ac72db60f810475b70afc26eb9f
Mark Rice <markmrice16@gmail.com>
2025-02-01 23:56:35 +0000

    entrypoints

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,3 +6,7 @@ source /app/venv/bin/activate
 
 # Run the main script
 exec python fetch_ebay_orders.py
+exec python generate_royal_mail_csv.py
+exec python upload_to_royalmail.py
+exec python get_tracking_numbers.py
+exec python update_ebay_orders.py
